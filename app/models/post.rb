1a786562fd58a12a8d07c44f153cf94df6f6186f
Sean <skabe89@gmail.com>
2021-01-16 15:33:37 -0500

    commit tree 43e3f474cd285a189cbd9b628509768d78409c57

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, presence: true, acceptance: {accept: ["Fiction", "Non-Fiction"]}
+  validates :content, presence:true, length: {minimum: 100}
 end
+
+# t.string "title"
+# t.string "category"
+# t.text "content"
